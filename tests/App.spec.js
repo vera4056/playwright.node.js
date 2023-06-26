1bcb2e75d0dd48025b48d5adcbdcd94d672e7fb5
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-06-26 21:51:17 +0300

    tests modified

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,21 +1,25 @@
-import { test, expect} from '@playwright/test';
+
 const { test, expect } = require("@playwright/test");
-const user = require("../user");
+const user = require("../user.js");
 
-
-test("positive test", async ({ page }) => {
-  await page.goto("https://netology.ru/free/management?modal=sign_in"); 
+test.describe('positive test', () => {
+test("successful authorizition", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in"); 
+  await page.screenshot({ path: 'screenshots/login-page.png'/*, fullPage: true*/});
   await page.getByPlaceholder('Email').click(); 
   await page.getByPlaceholder('Email').fill(user.email);
   await page.getByPlaceholder('Пароль').click();
   await page.getByPlaceholder('Пароль').fill(user.password);
   await page.getByTestId('login-submit-btn').click();
   const titleText = await page.textContent('h2');
-  await expect(page.getByRole('heading', {name: 'Мои курсы и профессии'})).toHaveText(titleText);
+  await expect(page.getByRole('heading', {name: 'Мои курсы и профессии' })).toHaveText(titleText);
+  await page.screenshot({ path: 'screenshots/my-courses-page2.png'/*, fullPage: true*/});
+  });
 });
 
- test("negative test", async ({ page }) => {
-  await page.goto("https://netology.ru/free/management?modal=sign_in");  
+ test.describe('negative test', () => {
+ test("not valid email", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");  
   await page.getByPlaceholder('Email').click(); 
   await page.getByPlaceholder('Email').fill("coco@yandex.ru"); 
   await page.getByPlaceholder('Пароль').click();
@@ -23,6 +27,7 @@ test("positive test", async ({ page }) => {
   await page.getByTestId('login-submit-btn').click();
   const titleText = await page.textContent('button');
   await expect(page.getByRole('login-error-hint', {name: 'Вы ввели неправильно логин или пароль'})).toHaveText(titleText);
+  await page.screenshot({ path: 'screenshots/my-courses-page2.png' /*, fullPage: true*/});
+   });
 });
-
   
